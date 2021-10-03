e1e21fcc0f4ec799dacce92c9a12a67af0bf845b
Tashtabanov Roman <rt.hagbo@gmail.com>
2021-10-03 13:12:43 +0500

    ДЗ №2, Задача №1

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,8 +1,23 @@
 // Задание 1
 function getArrayParams(arr) {
   let min, max, sum, avg;
+  min = Infinity;
+  max = -Infinity;
+  sum = 0;
 
-  // Ваш код
+  for (let i = 0; i < arr.length; i += 1) {
+    
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    
+    if (arr[i] > max) {
+      max = arr[i];
+    } 
+
+    sum = sum + arr[i];
+    avg = Number((sum / arr.length).toFixed(2));
+  }
 
   return { min: min, max: max, avg: avg };
 }
