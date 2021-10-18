3a60d4c4aa82048864cf6258e6717f0ff08d6f09
Tashtabanov Roman <rt.hagbo@gmail.com>
2021-10-18 19:47:37 +0500

    дз 3, задача №1

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,7 @@
 function compareArrays(arr1, arr2) {
   let result;
 
-  // Ваш код
+  result = arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index]);
 
   return result; // boolean
 }
