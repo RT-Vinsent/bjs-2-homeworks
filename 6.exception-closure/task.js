d1dd27fe04411dec9e5a3c7204a6672ab3a5a2ed
Tashtabanov Roman <rt.hagbo@gmail.com>
2021-10-26 04:20:38 +0500

    дз №6, задача №2

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -16,3 +16,37 @@ function validateCount(value) {
         return err;
     }
 }
+
+// Задача №2. Треугольник
+
+class Triangle {
+    constructor(a, b, c) {
+        if ((a + b < c) || (a + c < b) || (b + c < a)) {
+            throw new Error("Треугольник с такими сторонами не существует");
+        } else {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+    }
+
+    getPerimeter() {
+        return this.a + this.b + this.c;
+    }
+
+    getArea() {
+        let semiP =  this.getPerimeter() / 2;
+        return +(Math.sqrt(semiP * (semiP - this.a) * (semiP - this.b) * (semiP - this.c))).toFixed(3);
+    }
+}
+
+function getTriangle(a, b, c) {
+    try {
+        return new Triangle(a, b, c);
+    } catch (err) {
+        return {
+            getArea() {return "Ошибка! Треугольник не существует"},
+            getPerimeter() {return "Ошибка! Треугольник не существует"}
+        }
+    }
+}
