e9baf9a7751577ec007cd990600f5290680050ce
Tashtabanov Roman <rt.hagbo@gmail.com>
2021-10-27 16:24:19 +0500

    Домашнее задание к лекции 8 «Декораторы»

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,76 @@
+// Задача 1. Усовершенствуйте кэширующий декоратор
+
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+
+  function wrapper(...args) {
+    const hash = args.join(",");
+
+    let index = cache.findIndex((item) => item.hash === hash);
+
+    if (index !== -1) {
+      console.log("Из кэша: " + cache[index].value);
+      return ("Из кэша: " + cache[index].value);
+    } else {
+      let value = func(...args);
+      
+      cache.push({hash, value})
+
+      if (cache.length > 5) {
+        cache.shift();
+      }
+
+      console.log("Вычисляем: " + value);
+      return ("Вычисляем: " + value);
+    }
+  }
+  return wrapper;
 }
 
+// Задача 2. Фильтрация и преобразование массива
+function debounceDecoratorNew(func, ms) {
+  
+  let timeout;
+  let flag = false;
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+  function wrapper(...args) {
+
+    if (!flag) {
+      func.apply(this, args);
+      flag = true;
+    } else {
+      clearTimeout(timeout);
+      timeout = setTimeout(() => { 
+        func.apply(this, args);
+        flag = false
+      }, ms);
+      
+      //setTimeout(() => {console.log('проигнорировано')}, ms);
+    }
+  }
+  return wrapper;
 }
 
-function debounceDecorator2(func) {
-  // Ваш код
+// Задача 3. Усовершенствуйте debounceDecoratorNew
+function debounceDecorator2(func, ms) {
+  
+  let timeout;
+  let flag = false;
+  let counter = 0;
+
+  function wrapper(...args) {
+    counter++;
+    console.log(counter)
+    if (!flag) {
+      func.apply(this, args);
+      flag = true;
+    } else {
+      clearTimeout(timeout);
+      timeout = setTimeout(() => { 
+        func.apply(this, args);
+        flag = false
+      }, ms);
+    }
+  }
+  return wrapper;
 }
